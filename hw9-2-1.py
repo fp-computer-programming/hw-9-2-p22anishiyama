52f20ea7bab070a7499a2d3c86655ae3722f743a
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-01-13 09:50:31 -0500

    add hw9-2-1.py

--- hw9-2-1.py
+++ hw9-2-1.py
@@ -2,10 +2,10 @@
 
 def row_initials(last_initials, rows):
     for i, v in enumerate(rows):
-        if i < len(i):
-            rows[i] = "{0}, {1}.".format(rows, last_initials)
-        else:
-            return rows
+        rows[i] = "{0} {1}.".format(v, last_initials[i])
+    return rows
 
-print(row_initials[["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N", "N", "O", "P", "P", "X", "T", "S", "S", "P"]
-], [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin" "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]])
+initials = ["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N", "N", "O", "P", "P", "X", "T", "S", "S", "P"]
+roster = ["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan", "Sean", "Luis", "Ryan", "James", "Jack", "Aiden", "Ian", "Colin", "Tim", "Cam", "Larry", "Spencer", "Chris", "Ryan", "Nolan"]
+
+print(row_initials(initials, roster))
